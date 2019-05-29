e809cdffb2291430676cbfb9ea916fe2143532c0
Mary62442 <maria.bu62442@gmail.com>
2019-05-29 10:22:33 +0200

    Refactored function to accept all weights of neural network layers.

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 let math = require('mathjs');
 
 const sigmoid = (t) => {
-    return 1/(1+Math.pow(Math.E, -t));
+    return 1 / (1 + Math.pow(Math.E, -t));
+};
+const calculateLayerOutput = (input, weight) => {
+	let x = math.multiply(input,weight);
+    let o = x.map(e => sigmoid(e));
+	return o;
 };
 
-const calculateOutput = (input, weight) => {
-	let x = math.multiply(input,weight);
-    let output = x.map(e => sigmoid(e));
-	return output;
+const calculateAllOutputs = (input, weights) => {
+    let o = weights.reduce((tot,curr,i) => {
+        if (i === 0) {
+            let o1 = calculateLayerOutput(input, curr);
+            tot.push(o1);
+            return tot;
+        }
+        let oN = calculateLayerOutput(tot[i-1], curr);
+        tot.push(oN);
+        return tot;
+    },[]);
+    return o;
 };
 
 let input = math.matrix([0.9, 0.1, 0.8]);
+let weights = [math.matrix([[0.9,0.2,0.1], [0.3,0.8,0.5], [0.4,0.2,0.6]]), math.matrix([[0.3,0.6,0.8], [0.7,0.5,0.1], [0.5,0.2,0.9]])];
+let outputs = calculateAllOutputs(input, weights);
 
-let weight = math.matrix([[0.9,0.2,0.1], [0.3,0.8,0.5], [0.4,0.2,0.6]]);
-
-let output = calculateOutput(input, weight);
-
-console.log(output);
+console.log(outputs);
